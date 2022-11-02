c9215f9a24f3fa6ea9af33c63f313f1f4ee3c14a
Marcos <marcosrealbmata@gmail.com>
2022-11-02 17:29:44 -0300

    Commit init

---
+++ README.md
@@ -0,0 +1,29 @@
+# vectis
+
+Página empresarial para demonstração de serviços de contabilidade e possibilitar contato
+
+### Recommended IDE Setup
+
+[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur) + [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=Vue.vscode-typescript-vue-plugin).
+
+### Customize configuration
+
+See [Vite Configuration Reference](https://vitejs.dev/config/).
+
+### Project Setup
+
+```sh
+npm install
+```
+
+#### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+#### Compile and Minify for Production
+
+```sh
+npm run build
+```
---
+++ src/views/AboutView.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="about">
+    <h3>
+      <p>
+        • <span>Missão:</span> Democratizar a contabilidade consultiva para autônomos,
+        pequenas e médias empresas do Estado de Alagoas, oferecendo o melhor
+        atendimento de todo o Brasil.
+      </p>
+
+      <p>
+        • <span>Visão:</span> Ser a melhor empresa de contabilidade consultiva para o setor
+        de serviços do Estado de Alagoas.
+      </p>
+
+      <p>
+        • <span>Valores:</span> Simplicidade, Transparência, Honestidade,
+        Responsabilidade, Ética e Inovação. A identidade foi desenvolvida com as
+        características que são a peça chave da <b>marca:</b> Simplicidade,
+        Autoridade, Conança e Responsabilidade.
+      </p>
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+
+span{
+  font-weight: bold;
+}
+
+p{
+  margin: 15px;
+}
+
+
+</style>
