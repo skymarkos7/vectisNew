3a8efa0258ec4e4006008c9acf6818e47fa487d3
marcos <marcos@slice.global>
2022-12-15 15:47:04 -0300

    plausivél

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,16 @@ import HelloWorld from "./components/HelloWorld.vue";
 
 //  import { api } from './../boot/axios';
 //  import axios, {isCancel, AxiosError} from 'axios';
-    import axios from 'axios';
+import axios from "axios";
 //  const  axios = require('axios');
 //  const  axios = require('axios/dist/browser/axios.cjs');
 
 export default {
   data() {
     return {
-      id_boot: '5697282930:AAGs3om1DK9nJVxrrQgiWYpj2pgnz8MwDcc',
-      chat_id: '1169990427',
-      text: 'NovoVisitante'
+      id_boot: "5697282930:AAGs3om1DK9nJVxrrQgiWYpj2pgnz8MwDcc",
+      chat_id: "1169990427",
+      text: "NovoVisitante",
     };
   },
   mounted: function () {
@@ -22,34 +22,62 @@ export default {
     //   Seu ID:   915622513
     //   Meu ID:   1169990427
     //   Group ID: -861133006
-    
+
     // ipinfo.io/[endereço IP]?token= 7729640d392738
     // https://ipinfo.io/json
     // http://ip-api.com/json/
     //*****************************/
 
-    getLocale();
     async function getLocale() {
       try {
         const response_locale = await axios.get("http://ip-api.com/json/");
-        let country    = response_locale.data.country;
+        let country = response_locale.data.country;
         let regionName = response_locale.data.regionName;
-        let city       = response_locale.data.city;
-        let zip        = response_locale.data.zip;
-        let isp        = response_locale.data.isp;
-        let ip         = response_locale.data.query;
-        let text       = "<b>Você recebeu uma nova visita no site:</b> %0A %0A"+"<b>PAÍS:</b> "+country+"%0A"+"<b>ESTADO:</b> "+regionName+"%0A"+"<b>CIDADE:</b> "+city+"%0A"+"<b>PROVEDOR:</b> "+isp+"%0A"+"<b>IP:</b> "+ip;
+        let city = response_locale.data.city;
+        let zip = response_locale.data.zip;
+        let isp = response_locale.data.isp;
+        let ip = response_locale.data.query;
+        let text =
+          "<b>Você recebeu uma nova visita no site:</b> %0A %0A" +
+          "<b>PAÍS:</b> " +
+          country +
+          "%0A" +
+          "<b>ESTADO:</b> " +
+          regionName +
+          "%0A" +
+          "<b>CIDADE:</b> " +
+          city +
+          "%0A" +
+          "<b>PROVEDOR:</b> " +
+          "<b>CEP:</b> " +
+          zip +
+          "%0A" +
+          isp +
+          "%0A" +
+          "<b>IP:</b> " +
+          ip;
+        let text2 = response_locale.data;
 
-        let url = "https://api.telegram.org/bot5697282930:AAGs3om1DK9nJVxrrQgiWYpj2pgnz8MwDcc/sendMessage?chat_id=-861133006&text="+text+"&parse_mode=html";
+        const url =
+          "https://api.telegram.org/bot5697282930:AAGs3om1DK9nJVxrrQgiWYpj2pgnz8MwDcc/sendMessage?chat_id=-861133006&text=" +
+          text +
+          "&parse_mode=html";
+        //let url = "https://api.telegram.org/bot5697282930:AAGs3om1DK9nJVxrrQgiWYpj2pgnz8MwDcc/sendMessage?chat_id=1169990427&text="+text;
 
-        fetch(url)
-          } catch (error) {
-            console.error(error);
-          }
+        try{
+        const telegram = await axios.get(url);
+        console.log("enviou")
+        }catch{
+          console.log("Não enviou")
         }
 
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getLocale();
 
-     // let url = 'https://api.telegram.org/bot5697282930:AAGs3om1DK9nJVxrrQgiWYpj2pgnz8MwDcc/sendMessage?chat_id=1169990427&text=Novo_Visitante/';
+    // let url = 'https://api.telegram.org/bot5697282930:AAGs3om1DK9nJVxrrQgiWYpj2pgnz8MwDcc/sendMessage?chat_id=1169990427&text=Novo_Visitante/';
     // let url = `https://api.telegram.org/bot${this.id_boot}/sendMessage?chat_id=${this.chat_id}&text=${this.text}/`;
 
     // fetch(url)
